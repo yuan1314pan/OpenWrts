88730acd7163616b3f67a62e7937a1c871dbdafa
yuan1314pan <104306644+yuan1314pan@users.noreply.github.com>
2025-11-27 22:34:03 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 # Modify default system settings
 
-# 修改默认IP为192.168.10.1
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate 
+# 修改默认IP为192.168.100.1
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
-# Hello World
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-
-# passwall
+# 官方/第三方 feeds
+echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
 echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
-
-# iStore
 echo "src-git istore https://github.com/linkease/istore;main" >> feeds.conf.default
+
+# 新增 feeds
+echo "src-git tailscale https://github.com/berndog/lede-tailscale.git;main" >> feeds.conf.default
+echo "src-git ddnsgo https://github.com/linkease/ddns-go-feed.git;main" >> feeds.conf.default
+echo "src-git smartdns https://github.com/pymumu/openwrt-smartdns.git;main" >> feeds.conf.default
+echo "src-git socat https://github.com/immortalwrt/packages.git;packages" >> feeds.conf.default
+
+# 更新 feeds
+cd feeds
+../scripts/feeds update -a
+../scripts/feeds install -a
