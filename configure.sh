aae6b2f88870028488d7c1f0d08935a5df19bfb3
yuan1314pan <104306644+yuan1314pan@users.noreply.github.com>
2025-12-06 18:58:31 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -5,15 +5,12 @@
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
 cat <<'EOF' >> feeds.conf.default
-src-git helloworld https://github.com/fw876/helloworld
 src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main
 src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main
 src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main
 src-git istore https://github.com/linkease/istore;main
-src-git kenzo https://github.com/kenzok8/openwrt-packages
 src-git tailscale_lanrat git@github.com:lanrat/openwrt-tailscale-repo.git
 src-git docker https://github.com/openwrt/packages.git^refs/heads/master
-src-git tailscale git@github.com:berndog/lede-tailscale.git
 src-git ddnsgo git@github.com/linkease/ddns-go-feed.git
 src-git smartdns https://github.com/pymumu/openwrt-smartdns.git;main
 src-git socat https://github.com/immortalwrt/packages.git;packages
