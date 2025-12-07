24ebed484aa5a5909c021bcb5f175a3625c9cd90
yuan1314pan <104306644+yuan1314pan@users.noreply.github.com>
2025-12-07 16:53:35 +0800

    更新 configure.sh

--- configure.sh
+++ configure.sh
@@ -4,18 +4,18 @@
 # 修改默认IP为192.168.100.1
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
-{
-  echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main"
-  echo "src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main"
-  echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main"
-  echo "src-git istore https://github.com/linkease/istore;main"
-  echo "src-git tailscale_lanrat git@github.com:lanrat/openwrt-tailscale-repo.git"
-  echo "src-git docker https://github.com/openwrt/packages.git^refs/heads/master"
-  echo "src-git ddnsgo git@github.com/linkease/ddns-go-feed.git"
-  echo "src-git smartdns https://github.com/pymumu/openwrt-smartdns.git;main"
-  echo "src-git socat https://github.com/immortalwrt/packages.git;packages"
-  cat feeds.conf.default
-} | tee feeds.conf.default.tmp > feeds.conf.default
 
+cat <<'EOF' >> feeds.conf.default
+src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main
+src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main
+src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main
+src-git istore https://github.com/linkease/istore;main
+src-git tailscale_lanrat git@github.com:lanrat/openwrt-tailscale-repo.git
+src-git docker https://github.com/openwrt/packages.git^refs/heads/master
+src-git ddnsgo git@github.com/linkease/ddns-go-feed.git
+src-git smartdns https://github.com/pymumu/openwrt-smartdns.git;main
+src-git socat https://github.com/immortalwrt/packages.git;packages
+EOF
 
-
+./scripts/feeds update -a
+./scripts/feeds install -a
