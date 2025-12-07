776a44dec9ccd1b3743cfd811111769b22e2205f
yuan1314pan <104306644+yuan1314pan@users.noreply.github.com>
2025-12-07 16:40:14 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -17,7 +17,7 @@ sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_gener
   cat feeds.conf.default
 } | tee feeds.conf.default.tmp > feeds.conf.default
 
-cat feeds.conf.default
+
 
 
 
