b8f8e2ba0b7c395da9a00dadd653398e759ea37d
yuan1314pan <104306644+yuan1314pan@users.noreply.github.com>
2025-12-07 17:00:53 +0800

    更新 configure.sh

--- configure.sh
+++ configure.sh
@@ -17,5 +17,13 @@ src-git smartdns https://github.com/pymumu/openwrt-smartdns.git;main
 src-git socat https://github.com/immortalwrt/packages.git;packages
 EOF
 
+# 移除 openwrt feeds 自带的核心库
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
+
+# 移除 openwrt feeds 过时的luci版本
+rm -rf feeds/luci/applications/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a
