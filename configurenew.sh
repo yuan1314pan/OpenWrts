80412df8a54768c47f46749b2097d2c2a3d4e6ae
yuan1314pan <104306644+yuan1314pan@users.noreply.github.com>
2025-12-10 18:37:27 +0800

    Update configurenew.sh

--- configurenew.sh
+++ configurenew.sh
@@ -8,14 +8,14 @@
 cat > feeds.tmp <<'EOF'
 src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main
 src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main
-src-git helloworld https://github.com/fw876/helloworld
+#src-git helloworld https://github.com/fw876/helloworld
 #src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main
 src-git istore https://github.com/linkease/istore;main
 src-git kenzo https://github.com/kenzok8/openwrt-packages
-src-git tailscale_lanrat git@github.com:lanrat/openwrt-tailscale-repo.git
+#src-git tailscale_lanrat git@github.com:lanrat/openwrt-tailscale-repo.git
 #src-git docker https://github.com/openwrt/packages.git^refs/heads/master
-src-git tailscale git@github.com:berndog/lede-tailscale.git
-src-git ddnsgo git@github.com/linkease/ddns-go-feed.git
+#src-git tailscale git@github.com:berndog/lede-tailscale.git
+#src-git ddnsgo git@github.com/linkease/ddns-go-feed.git
 src-git smartdns https://github.com/pymumu/openwrt-smartdns.git;main
 src-git socat https://github.com/immortalwrt/packages.git;packages
 EOF
