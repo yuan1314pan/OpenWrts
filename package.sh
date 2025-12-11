a9e5ecf221712ac94cbe8b2648abc85178f88292
yuan1314pan <104306644+yuan1314pan@users.noreply.github.com>
2025-12-11 19:00:43 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-git clone --depth 1 https://github.com/destan19/OpenAppFilter package/otherapp/OpenAppFilter
 git clone --depth 1 https://github.com/zzsj0928/luci-app-pushbot package/otherapp/luci-app-pushbot
 git clone --depth 1 https://github.com/sirpdboy/luci-app-wizard package/otherapp/luci-app-wizard
 git clone --depth 1 https://github.com/sirpdboy/luci-app-advancedplus package/otherapp/luci-app-advancedplus
